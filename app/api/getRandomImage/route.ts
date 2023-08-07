e78181575a9768c3a90e1b73f3a14cf5390f6718
huan5678 <huan5678@gmail.com>
2023-08-07 17:06:00 +0800

    [fix] getRandomImage fix bug

--- app/api/getRandomImage/route.ts
+++ app/api/getRandomImage/route.ts
@@ -5,24 +5,11 @@ export async function GET(request: Request) {
   const directoryPath = path.join(process.cwd(), "public", "images");
   const files = await fs.promises.readdir(directoryPath);
   const randomImage = files[Math.floor(Math.random() * files.length)];
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  return new Response(JSON.stringify({ name: `/images/${randomImage}` }), {
-    status: 200,
-    headers: {
-      "content-type": "text/plain",
-=======
+
   return new Response(JSON.stringify({name: `/images/${randomImage}`}), {
     status: 200,
     headers: {
       'content-type': 'text/plain',
->>>>>>> Stashed changes
-=======
-  return new Response(JSON.stringify({name: `/images/${randomImage}`}), {
-    status: 200,
-    headers: {
-      'content-type': 'text/plain',
->>>>>>> Stashed changes
     },
   });
 }
